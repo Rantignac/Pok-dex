3cd2828b328c0a0e0da5a668ccd3823a224dd04d
RegisAntignac <antignac.regis@gmail.com>
2019-06-04 11:45:14 +0200

    Add config + dockerfile

---
+++ app.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const app = express();
+const mongodb = require('mongodb');
+
+const config = require('./config/db');
+const PORT = 3000;
+const client = mongodb.MongoClient;
+
+client.connect(config.DB, function(err, db) {
+    if(err) {
+        console.log('database is not connected')
+    }
+    else {
+        console.log('connected!!')
+    }
+});
+
+app.get('/', function(req, res) {
+    res.json({"hello": "world"});
+});
+
+app.listen(PORT, function(){
+    console.log('Your node js server is running on PORT:',PORT);
+});
