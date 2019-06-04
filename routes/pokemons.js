323a8f14bb00eb5b567efc477f23bf82951554d8
RegisAntignac <antignac.regis@gmail.com>
2019-06-04 17:54:28 +0200

    Add routes

---
+++ routes/pokemons.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+var Pokemon = require('../models/Pokemon');
+router.get('/', (req, res)=> {
+    Pokemon.find({}).populate('types').then( pokemons => {
+        res.render('pokemons/index.html', {pokemons: pokemons})
+    });
+});
+module.exports = router;
