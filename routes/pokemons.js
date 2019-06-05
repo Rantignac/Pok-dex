1c7d8f7a31e5b513febea9af4744b1163c1f1e98
RegisAntignac <antignac.regis@gmail.com>
2019-06-05 14:13:33 +0200

    Add a create and edit form

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -1,16 +1,43 @@
 const router = require('express').Router();
 var Pokemon = require('../models/Pokemon');
+var Types = require('../models/Type');
+
 router.get('/', (req, res) => {
     Pokemon.find({}).populate('types').then(pokemons => {
-        res.render('pokemons/index.html', {pokemons: pokemons})
+        res.render('pokemons/index.html', {
+            pokemons: pokemons
+        })
+    });
+});
+
+router.get('/new', (req, res) => {
+    Types.find({}).then((types) => {
+        var pokemon = new Pokemon();
+        res.render('pokemons/edit.html', {
+            pokemon: pokemon,
+            types: types
+        });
+    });
+});
+
+router.get('/edit/:id', (req, res) => {
+    Types.find({}).then((types) => {
+        Pokemon.findById(req.params.id).then(pokemon => {
+            res.render('pokemons/edit.html', {
+                pokemon: pokemon,
+                types: types
+            });
+        });
     });
 });
 
 router.get('/:id', (req, res) => {
     Pokemon.findById(req.params.id).populate('types').then(pokemon => {
-        res.render('pokemons/show.html', {pokemon: pokemon});
-    }),
-    err => res.status(500).send(err);
+            res.render('pokemons/show.html', {
+                pokemon: pokemon
+            });
+        }),
+        err => res.status(500).send(err);
 });
 
 module.exports = router;
