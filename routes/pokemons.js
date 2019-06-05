1820dd23b4500ab0cd3105485c6333e1ec0f2d1e
RegisAntignac <antignac.regis@gmail.com>
2019-06-05 10:31:04 +0200

    Add pokemon details

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
 var Pokemon = require('../models/Pokemon');
-router.get('/', (req, res)=> {
-    Pokemon.find({}).populate('types').then( pokemons => {
+router.get('/', (req, res) => {
+    Pokemon.find({}).populate('types').then(pokemons => {
         res.render('pokemons/index.html', {pokemons: pokemons})
     });
 });
+
+router.get('/:id', (req, res) => {
+    Pokemon.findById(req.params.id).populate('types').then(pokemon => {
+        res.render('pokemons/show.html', {pokemon: pokemon});
+    }),
+    err => res.status(500).send(err);
+});
+
 module.exports = router;
